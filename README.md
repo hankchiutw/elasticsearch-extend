58a2ba6d8ee3c8cd87bf531c036a483f2cb0ae78
Hank <hankchiu.tw@gmail.com>
2016-06-28 04:51:30 +0000

    improve README.md and APIs

--- README.md
+++ README.md
@@ -9,8 +9,53 @@ Extended official [elasticsearch](https://www.npmjs.com/package/elasticsearch) m
 
 ### How to use
 
+Init es connection:
+```js
+const es = require('elasticsearch-extend');
+
+es.connect({
+    host: 'localhost:9200',
+    log: 'info'
+})
+.then(function(){ ... })
+.catch(function(error){ ... });
+```
+
+Once connected, register a model:
+```js
+const UserModel = es.model('user', {
+    username: { type: "string"},
+    phone: { type: "string"}
+});
+```
+
+Get a registered model:
+```js
+const UserModel = es.model('user');
+```
 
 ### APIs
 
+##### es.connect(params)
+> Connect to an elasticsearch server
+
+##### es.isConnected
+> Indicate wheather elasticsearch connected or not
+
+##### es.model(modelName, mapping)
+> Register a model
+
+##### es.model(modelName)
+> Get a model
+
+#### Model APIs
+##### model.createOne(params)
+> Create one document
+
+##### model.list(params)
+> List documents
+
 
 ### ToDos
+- More model APIs
+- Document APIs
--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const Model = require('./lib/Model');
 Object.assign(es, {
     isConnected: false,
     connect,
-    model
+    model,
+    _models: {}
 });
 
 module.exports = es;
@@ -37,14 +38,23 @@ function connect(params){
 
 
 /**
- * Create an index as a model instance
+ * Register/Get an index as a model instance
+ * @params {String} modelName
+ * @params {Object} mapping If not set, try to return registered model
  */
 function model(modelName, mapping){
     if(!es.isConnected) throw new Error('(elasticsearch-extend) not connected.');
+    if(mapping === undefined){
+        if(!es._models[modelName]) throw new Error(`(elasticsearch-extend) model ${modelName} not registered.`);
+        return es._models[modelName];
+    }
+    else if(es._models[modelName]) throw new Error(`(elasticsearch-extend) model ${modelName} already registered.`);
 
     // delayed save to db
     const model = new Model({ modelName, mapping});
     co(model.save()).catch(console.error);
+
+    es._models[modelaName] = model;
 
     // return instance
     return model;
